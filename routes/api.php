98604b8fe4f532e75bf6772cb701ac1ba3f708b9
martoast <alexmartos96@gmail.com>
2025-07-08 00:59:08 -0700

    remove extra and ready for just api

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    // Add your API routes here
+});
